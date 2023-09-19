efe3f039571b9ca36ce9e99c981873eecfa62262
adsouza <arkievdsouza@sydney.edu.au>
2023-09-19 14:46:31 +1000

    test to run full pipeline

--- T1_pipeline_pydra.py
+++ T1_pipeline_pydra.py
@@ -13,8 +13,7 @@ from pydra.tasks.fastsurfer.latest import fastsurfer
 output_path = '/Users/arkievdsouza/git/t1-pipeline/working-dir'
 
 # Define the input_spec for the workflow
-input_spec = {"t1w": NiftiGz, "fs_license": File, "sub_ID": str}
-# input_spec = {"t1w": NiftiGz, "norm_image": NiftiGz, "aparc_aseg": NiftiGz, "FS_dir": Directory, "default_file": File, "freesurfer_LUT": File}
+input_spec = {"t1w": NiftiGz, "fs_license": File, "sub_ID": str, "default_file": File, "freesurfer_LUT": File}
 output_spec = {"fTT_image": ImageFormat,"vis_image": ImageFormat,  "parc_image": ImageFormat}
 
 # Create a workflow 
@@ -26,12 +25,15 @@ wf.add(
         T1_files=wf.lzin.t1w, 
         fs_license=wf.lzin.fs_license,
         subject_id=wf.lzin.sub_ID,
-        threads=24,
+        threads=7,
         parallel=True,
         name="FastSurfer_task",
-        py="python3.11"
-        # no_cuda=True
+        py="python3.11",
+        norm_img="norm.nii.gz",
+        aparcaseg="aparcaseg.nii.gz",
+        
     )    
+
 )
 
 # #################################################
@@ -63,30 +65,30 @@ wf.add(
 # # DK parcellation #
 # ###################
 
-# # relabel segmenetation to integers indexed from 1 to 84
-# wf.add(
-#     labelconvert(
-#         path_in=wf.lzin.aparc_aseg,
-#         lut_in=wf.lzin.freesurfer_LUT,
-#         lut_out=wf.lzin.default_file,
-#         path_out="nodes.mif",
-#         name="LabelConvert_task"
-#     )
-# )
+# relabel segmenetation to integers indexed from 1 to 84
+wf.add(
+    labelconvert(
+        path_in=wf.FastSurfer_task.lzout.aparcaseg_img,
+        lut_in=wf.lzin.freesurfer_LUT,
+        lut_out=wf.lzin.default_file,
+        path_out="nodes.mif",
+        name="LabelConvert_task"
+    )
+)
 
-# # # Replace FreeSurfer’s estimates of sub-cortical grey matter structures with estimates from FSL’s FIRST tool
-# wf.add(
-#     labelsgmfix(
-#         parc=wf.LabelConvert_task.lzout.path_out, 
-#         t1=wf.lzin.norm_image,
-#         lut=wf.lzin.default_file,
-#         output="parcellation_image_DK_unregistered.mif",
-#         name="SGMfix_task",
-#         nocleanup=True,
-#         premasked=True,
-#         sgm_amyg_hipp=True
-#     )
-# )
+# # Replace FreeSurfer’s estimates of sub-cortical grey matter structures with estimates from FSL’s FIRST tool
+wf.add(
+    labelsgmfix(
+        parc=wf.LabelConvert_task.lzout.path_out, 
+        t1=wf.FastSurfer_task.lzout.norm_img,
+        lut=wf.lzin.default_file,
+        output="parcellation_image_DK_unregistered.mif",
+        name="SGMfix_task",
+        nocleanup=True,
+        premasked=True,
+        sgm_amyg_hipp=True
+    )
+)
 
 ###################
 ## WORKFLOW SETUP #
@@ -94,7 +96,7 @@ wf.add(
 
 wf.set_output(("fTT_image", wf.fTTgen_task.lzout.output.cast(ImageFormat)))
 wf.set_output(("vis_image", wf.fTTvis_task.lzout.output.cast(ImageFormat)))
-# wf.set_output(("parc_image", wf.SGMfix_task.lzout.output.cast(ImageFormat)))
+wf.set_output(("parc_image", wf.SGMfix_task.lzout.output.cast(ImageFormat)))
 
 # ## Execute the workflow (FastSurfer, NIF data)
 # result = wf(
@@ -109,30 +111,10 @@ result = wf(
     t1w="/Users/arkievdsouza/Documents/100307/100307_FastSurfer/mri/orig.nii.gz",
     fs_license="/Users/arkievdsouza/Desktop/FastSurferTesting/ReferenceFiles/FS_license.txt",
     sub_ID="100307",
+    default_file="/Users/arkievdsouza/Desktop/FastSurferTesting/ReferenceFiles/fs_default.txt",
+    freesurfer_LUT="/Users/arkievdsouza/Desktop/FastSurferTesting/ReferenceFiles/FreeSurferColorLUT.txt",
     plugin="serial",
 )
-
-# # Execute the workflow - NIF data
-# result = wf(
-#     t1w="/Users/arkievdsouza/Documents/NIFdata/ds000114/sub-01/ses-retest/anat/sub-01_ses-retest_T1w.nii.gz",
-#     norm_image="/Users/arkievdsouza/Documents/sub-01_ses_retest/mri/norm.nii.gz",
-#     aparc_aseg="/Users/arkievdsouza/Documents/sub-01_ses_retest/mri/aparc+aseg.nii.gz",
-#     FS_dir="/Users/arkievdsouza/Documents/sub-01_ses_retest/",
-#     default_file="/Users/arkievdsouza/Desktop/FastSurferTesting/ReferenceFiles/fs_default.txt",
-#     freesurfer_LUT="/Users/arkievdsouza/Desktop/FastSurferTesting/ReferenceFiles/FreeSurferColorLUT.txt",
-#     plugin="serial",
-# )
-
-# Execute the workflow - HCP data
-# result = wf(
-#     t1w="/Users/arkievdsouza/Documents/100307/100307_FastSurfer/mri/orig.nii.gz",
-#     norm_image="/Users/arkievdsouza/Documents/100307/100307_FastSurfer/mri/norm.nii.gz",
-#     aparc_aseg="/Users/arkievdsouza/Documents/100307/100307_FastSurfer/mri/aparc+aseg.nii.gz",
-#     FS_dir="/Users/arkievdsouza/Documents/100307/100307_FastSurfer/",
-#     default_file="/Users/arkievdsouza/Desktop/FastSurferTesting/ReferenceFiles/fs_default.txt",
-#     freesurfer_LUT="/Users/arkievdsouza/Desktop/FastSurferTesting/ReferenceFiles/FreeSurferColorLUT.txt",
-#     plugin="serial",
-# )
 
 # print(f"Processed output generated at '{result.output.dwi_preproc}'")
 
