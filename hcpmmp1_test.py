22889908212810463950d7bb2ccd88be77ee7361
adsouza <arkievdsouza@sydney.edu.au>
2023-11-08 11:39:20 +1100

    updated aparc2aseg

--- hcpmmp1_test.py
+++ hcpmmp1_test.py
@@ -1,81 +1,57 @@
 from pydra import Workflow, mark
 import os
 import pydra.tasks.freesurfer.MRISurf2Surf
-import pydra.tasks.freesurfer.MRIAparc2Aseg
+from src.pydra.tasks.freesurfer.auto import aparc_2_aseg
 
 # Define some filepaths
 freesurfer_home='/Applications/freesurfer/'
 mrtrix_lut_dir='/Users/arkievdsouza/git/mrtrix3/share/mrtrix3/labelconvert/' 
-output_path = '/Users/arkievdsouza/git/t1-pipeline/working-dir/v2_tests/'
+output_path = '/Users/arkievdsouza/git/t1-pipeline/working-dir/hcpmmp1_test/'
 
 # Define the input values using input_spec
 input_spec = {"FS_dir": str, "parcellation": str} 
-wf = Workflow(name='hcpmmp1_parcellation', input_spec=input_spec,cache_dir=output_path) 
+wf = Workflow(name='hcpmmp1_parcellation', input_spec=input_spec, cache_dir=output_path) 
 
 @mark.task
 @mark.annotate({"parcellation": str, "FS_dir": str, "freesurfer_home": str,"return": {"fs_parc_image": str, "parc_lut_file": str, "mrtrix_lut_file": str, "output_parcellation_filename": str, "lh_white": str, "rh_white":str, "lh_pial": str, "rh_pial": str, "lh_ribbon": str, "rh_ribbon": str, "ribbon": str, "lh_annotation": str, "rh_annotation": str}})
-def hcpmmp1_conversion(parcellation: str, FS_dir: str, freesurfer_home: str):
-    if parcellation == 'hcpmmp1':
-        fs_parc_image='N/A for this parcellation scheme'     
-        parc_lut_file = os.path.join(mrtrix_lut_dir,'hcpmmp1_original.txt')
-        mrtrix_lut_file = os.path.join(mrtrix_lut_dir,'hcpmmp1_ordered.txt')
-        output_parcellation_filename ='aparc.HCPMMP1+aseg.mgz'
-        lh_white= os.path.join(FS_dir,"surf","lh.white")
-        rh_white= os.path.join(FS_dir,"surf","rh.white")
-        lh_pial= os.path.join(FS_dir,"surf","lh.pial")
-        rh_pial= os.path.join(FS_dir,"surf","rh.pial")
-        lh_ribbon= os.path.join(FS_dir,"mri","lh.ribbon.mgz")
-        rh_ribbon= os.path.join(FS_dir,"mri","rh.ribbon.mgz")
-        ribbon= os.path.join(FS_dir,"mri","ribbon.mgz")
-        lh_annotation= os.path.join(FS_dir,"label","lh.aparc.annot")
-        rh_annotation= os.path.join(FS_dir,"label","rh.aparc.annot")
+def join_hcpmmp1(parcellation: str, FS_dir: str, freesurfer_home: str):
+    # if parcellation == 'hcpmmp1':
+    fs_parc_image='N/A for this parcellation scheme'     
+    parc_lut_file = os.path.join(mrtrix_lut_dir,'hcpmmp1_original.txt')
+    mrtrix_lut_file = os.path.join(mrtrix_lut_dir,'hcpmmp1_ordered.txt')
+    output_parcellation_filename ='aparc.HCPMMP1+aseg.mgz'
+    lh_white= os.path.join(FS_dir,"surf","lh.white")
+    rh_white= os.path.join(FS_dir,"surf","rh.white")
+    lh_pial= os.path.join(FS_dir,"surf","lh.pial")
+    rh_pial= os.path.join(FS_dir,"surf","rh.pial")
+    lh_ribbon= os.path.join(FS_dir,"mri","lh.ribbon.mgz")
+    rh_ribbon= os.path.join(FS_dir,"mri","rh.ribbon.mgz")
+    ribbon= os.path.join(FS_dir,"mri","ribbon.mgz")
+    lh_annotation= os.path.join(FS_dir,"label","lh.aparc.annot")
+    rh_annotation= os.path.join(FS_dir,"label","rh.aparc.annot")
+    print("lh_pial: ", lh_pial)
+    print("lh_annotation: ", lh_annotation)    
     return fs_parc_image, parc_lut_file, mrtrix_lut_file, output_parcellation_filename, lh_white, rh_white, lh_pial, rh_pial, lh_ribbon, rh_ribbon, ribbon, lh_annotation, rh_annotation
+
+wf.add(join_hcpmmp1(parcellation=wf.lzin.parcellation, name="join_task_hcpmmp1"))
 
 wf.add(
   aparc_2_aseg(
         name="aparc2aseg_task",
         subject_id="FS_outputs",
-        out_file=output_parcellation_filename,
-        lh_white=lh_white,
-        rh_white=rh_white,
-        lh_pial=lh_pial,
-        rh_pial=rh_pial,
-        lh_ribbon=lh_ribbon,
-        rh_ribbon=rh_ribbon,
-        ribbon=ribbon,
-        lh_annotation=lh_annotation,
-        rh_annotation=rh_annotation,
+        out_file=wf.join_task_hcpmmp1.lzout.output_parcellation_filename,
+        lh_white=wf.join_task_hcpmmp1.lzout.lh_white,
+        rh_white=wf.join_task_hcpmmp1.lzout.rh_white,
+        lh_pial=wf.join_task_hcpmmp1.lzout.lh_pial,
+        rh_pial=wf.join_task_hcpmmp1.lzout.rh_pial,
+        lh_ribbon=wf.join_task_hcpmmp1.lzout.lh_ribbon,
+        rh_ribbon=wf.join_task_hcpmmp1.lzout.rh_ribbon,
+        ribbon=wf.join_task_hcpmmp1.lzout.ribbon,
+        lh_annotation=wf.join_task_hcpmmp1.lzout.lh_annotation,
+        rh_annotation=wf.join_task_hcpmmp1.lzout.rh_annotation,
     )
 )
   
-
-
-
-
-    # elif shared.parcellation == 'hcpmmp1':
-    #             parc_image_path = os.path.join(parc_image_path,
-    #                                            'aparc.HCPMMP1+aseg.mgz')
-    #             for index, hemi in enumerate(['l', 'r']):
-    #                 run.command('mri_surf2surf '
-    #                             '--srcsubject fsaverage '
-    #                             '--trgsubject freesurfer '
-    #                             '--hemi ' + hemi + 'h '
-    #                             '--sval-annot '
-    #                             + shared.hcpmmp1_annot_paths[index]
-    #                             + ' --tval '
-    #                             + os.path.join('freesurfer',
-    #                                            'label',
-    #                                            hemi + 'h.HCPMMP1.annot'),
-    #                             env=env)
-    #             run.command('mri_aparc2aseg '
-    #                         '--s freesurfer '
-    #                         '--old-ribbon '
-    #                         '--annot HCPMMP1 '
-    #                         '--o ' + parc_image_path,
-    #                         env=env)
-
-
-wf.add(hcpmmp1_conversion(parcellation=wf.lzin.parcellation, name="hcpmmp1_conversion_task"))
 
 
 ########################
